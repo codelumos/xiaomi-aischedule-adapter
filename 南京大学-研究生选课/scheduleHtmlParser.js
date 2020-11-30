65398c83d6983f9d55c1a8c5f34a081504839e42
HaoNShi <haonshi@qq.com>
2020-11-30 10:32:21 +0800

    优化 规范代码和注释

--- 南京大学-研究生选课/scheduleHtmlParser.js
+++ 南京大学-研究生选课/scheduleHtmlParser.js
@@ -3,22 +3,22 @@
  * <p>Description: 可使用正则匹配；可使用解析dom匹配，工具内置了$，跟jquery使用方法一样，直接用就可以了，参考：https://juejin.im/post/5ea131f76fb9a03c8122d6b9</p>
  *
  * @author  史浩楠
- * @date    2020-11-29
- * @version 1.3
+ * @date    2020-11-30
+ * @version 1.3.1
  */
 
 /**
- * 周数转换
+ * 解析周数
  *
- * @param  String  str 周数字符串
+ * @param  str 周数字符串
  * @return Array[]
  */
 function parseWeeks(str) {
     let result = []
-    var arr = str.split(',') // 逗号分隔
+    let arr = str.split(',') // 逗号分隔周数片段
     for (j = 0; j < arr.length; j++) {
         part = arr[j]
-        if (part.search("-") != -1) { // x-x周的形式
+        if (part.search("-") !== -1) { // x-x周的形式
             let begin = parseInt(part.split('-')[0])
             let end = parseInt(part.split('-')[1])
             for (let i = begin; i < end + 1; i++) {
@@ -32,79 +32,44 @@ function parseWeeks(str) {
 }
 
 /**
- * 课时转换
- *
- * @param  Array[] array 课时列表
- * @return Array[]
- */
-function parseSections(array) {
-    let result = []
-    for (var i = 0; i < array.length; i++) {
-        result.push({section: array[i]})
-    }
-    return result
-}
-
-/**
- * 课程信息JSON转换
- *
- * @param  Object[] list 课程信息列表
- * @return JSON
- */
-function course2JSON(list) {
-    let result = []
-    for (var i = 0; i < list.length; i++) {
-        let course = {}
-        course.name = list[i].name
-        course.position = list[i].position
-        course.teacher = list[i].teacher
-        course.weeks = list[i].weeks
-        course.day = list[i].day
-        course.sections = parseSections(list[i].sections)
-        result.push(course)
-    }
-    return result
-}
-
-/**
  * 解析课程信息
  *
- * @param  String   html 课表html
- * @return Object[]
+ * @param  html 课表html
+ * @return Array[{},{}]
  */
 function parseCourse(html) {
-    const $ = cheerio.load(html, {decodeEntities: false});
+    const $ = cheerio.load(html, {decodeEntities: false}); // 避免中文乱码
     let course = []
     // 遍历所有课程单元格div
     $('#wdkbTable > tbody > tr > td > div').each(function () {
         // 获取课程信息
-        var raw_weeks = $(this).find("div:nth-child(1)").text()
-        var raw_name = $(this).find("div:nth-child(2)").text()
-        var teacher = $(this).find("div:nth-child(3)").text()
-        var position = $(this).find("div:nth-child(4)").text()
-        var day = String($(this).parent().attr("data-week"))
-        var sections = String($(this).parent().attr("data-begin-unit"))
+        let raw_weeks = $(this).find("div:nth-child(1)").text()
+        let raw_name = $(this).find("div:nth-child(2)").text()
+        let teacher = $(this).find("div:nth-child(3)").text()
+        let position = $(this).find("div:nth-child(4)").text()
+        let day = String($(this).parent().attr("data-week"))
+        let sections = String($(this).parent().attr("data-begin-unit"))
         // 转换课程信息格式
-        var weeks = parseWeeks(raw_weeks)
-        var name = raw_name.replace(/\(.*?\)/g, '') // 移除字符串中的所有()括号（包括其内容）
-        var token = name + weeks + day
+        let weeks = parseWeeks(raw_weeks)
+        let name = raw_name.replace(/\(.*?\)/g, '') // 移除字符串中的所有()括号（包括其内容）
+        let token = name + weeks + day // 用来唯一标识一门课程信息
         // 判断是否记录过该门课程
-        var flag = -1
-        for (var i = 0; i < course.length; i++) {
-            if (course[i].token == token) {
+        let flag = -1
+        for (let i = 0; i < course.length; i++) {
+            if (course[i].token === token) {
                 flag = i
             }
         }
-        if (flag == -1) {
+        if (flag === -1) {
             // 记录新的课程信息
-            var obj = {
+            let obj = {
                 name: name,
                 position: position,
                 teacher: teacher,
                 weeks: weeks,
                 day: day,
                 sections: [sections],
-                token: name + weeks + day
+                token: token
             }
             course.push(obj)
         } else {
@@ -118,16 +83,16 @@ function parseCourse(html) {
 /**
  * 解析课时信息
  *
- * @param  String  html 课表html
- * @return JSON
+ * @param  html 课表html
+ * @return Array[{},{}]
  */
 function parseSection(html) {
     let result = []
-    var i = 0
+    let i = 0
     // 遍历所有课程单元格
     $('#wdkbTable').find('tr').each(function () {
-        var time = $(this).find("td:nth-child(2)").text()
-        if (i != 0) {
+        let time = $(this).find("td:nth-child(2)").text()
+        if (i !== 0) {
             let section = {}
             section.section = i
             section.startTime = time.split('~')[0]
@@ -140,17 +105,52 @@ function parseSection(html) {
 }
 
 /**
- * 输入课程页面的HTML字符串，提取课程信息，按约定的格式输出JSON
+ * 转换课时信息为约定的JSON格式
  *
- * @param  String html 上一步函数获取到的html
- * @return JSON
+ * @param  array 课时列表
+ * @return Array[]
+ */
+function sectionsFormat(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        result.push({section: array[i]})
+    }
+    return result
+}
+
+/**
+ * 转换课程信息为约定的JSON格式
+ *
+ * @param  list 课程信息列表
+ * @return Array[]
+ */
+function courseFormat(list) {
+    let result = []
+    for (let i = 0; i < list.length; i++) {
+        let course = {}
+        course.name = list[i].name
+        course.position = list[i].position
+        course.teacher = list[i].teacher
+        course.weeks = list[i].weeks
+        course.day = list[i].day
+        course.sections = sectionsFormat(list[i].sections)
+        result.push(course)
+    }
+    return result
+}
+
+/**
+ * 输入课程页面的HTML字符串，提取课程信息，按约定的格式输出JSON字符串
+ *
+ * @param  html 上一步函数获取到的html
+ * @return JSON{}
  */
 function scheduleHtmlParser(html) {
     let raw_course = []
     let course = []
     let section = []
     raw_course = parseCourse(html)
-    course = course2JSON(raw_course)
+    course = courseFormat(raw_course)
     section = parseSection(html)
     return {courseInfos: course, sectionTimes: section}
 }
--- 南京大学-网上办事服务大厅-我的课表/scheduleHtmlParser.js
+++ 南京大学-网上办事服务大厅-我的课表/scheduleHtmlParser.js
@@ -3,22 +3,22 @@
  * <p>Description: 可使用正则匹配；可使用解析dom匹配，工具内置了$，跟jquery使用方法一样，直接用就可以了，参考：https://juejin.im/post/5ea131f76fb9a03c8122d6b9</p>
  *
  * @author  史浩楠
- * @date    2020-11-25
- * @version 1.0
+ * @date    2020-11-30
+ * @version 1.0.1
  */
 
 /**
- * 周数转换
+ * 解析周数
  *
- * @param  String  str 周数字符串
+ * @param  str 周数字符串
  * @return Array[]
  */
 function parseWeeks(str) {
     let result = []
-    var arr = str.split(',') // 逗号分隔
-    for(j = 0; j < arr.length; j++) {
+    let arr = str.split(',') // 逗号分隔周数片段
+    for (j = 0; j < arr.length; j++) {
         part = arr[j]
-        if (part.search("-") != -1 ) { // x-x周的形式
+        if (part.search("-") !== -1) { // x-x周的形式
             let begin = parseInt(part.split('-')[0])
             let end = parseInt(part.split('-')[1])
             for (let i = begin; i < end + 1; i++) {
@@ -32,88 +32,54 @@ function parseWeeks(str) {
 }
 
 /**
- * 课时转换
- *
- * @param  Array[] array 课时列表
- * @return Array[]
- */
-function parseSections(array) {
-    let result = []
-    for (var i = 0; i < array.length; i++) {
-        result.push({ section: array[i] })
-    }
-    return result
-}
-
-/**
- * 课程信息JSON转换
- *
- * @param  Object[] list 课程信息列表
- * @return JSON
- */
-function course2JSON(list) {
-    let result = []
-    for (var i = 0; i < list.length; i++) {
-        let course = {}
-        course.name = list[i].name
-        course.position = list[i].position
-        course.teacher = list[i].teacher
-        course.weeks = list[i].weeks
-        course.day = list[i].day
-        course.sections = parseSections(list[i].sections)
-        result.push(course)
-    }
-    return result
-}
-
-/**
  * 解析课程信息
  *
- * @param  String   html 课表html
- * @return Object[]
+ * @param  html 课表html
+ * @return Array[{},{}]
  */
 function parseCourse(html) {
-    const $ = cheerio.load(html, { decodeEntities: false });
+    const $ = cheerio.load(html, {decodeEntities: false}); // 避免中文乱码
     let course = []
     // 遍历所有课程单元格
-    $('#jsTbl_01').find('.arrage').each(function() {
+    $('#jsTbl_01').find('.arrage').each(function () {
         // 存在课程的单元格
-        if ($(this).parent().css('display') != 'none') {
+        if ($(this).parent().css('display') !== 'none') {
             // 获取课程信息
-            var raw_weeks = $(this).find("div:nth-child(1)").text()
-            var raw_name = $(this).find("div:nth-child(2)").text()
-            var teacher = $(this).find("div:nth-child(3)").text()
-            var position = $(this).find("div:nth-child(4)").text()
-            var day = $(this).parent().attr("xq")
-            var sectionBegin = $(this).parent().attr("jcxq").split('-')[0]
-            var sectionLength = $(this).parent().attr("rowspan")
+            let raw_weeks = $(this).find("div:nth-child(1)").text()
+            let raw_name = $(this).find("div:nth-child(2)").text()
+            let teacher = $(this).find("div:nth-child(3)").text()
+            let position = $(this).find("div:nth-child(4)").text()
+            let day = $(this).parent().attr("xq")
+            let sectionBegin = $(this).parent().attr("jcxq").split('-')[0]
+            let sectionLength = $(this).parent().attr("rowspan")
             if (sectionLength == null) {
                 sectionLength = 1
             }
-            var sections = []
+            let sections = []
             for (let i = 0; i < sectionLength; i++) {
                 sections.push(parseInt(sectionBegin) + i)
             }
             // 转换课程信息格式
-            var weeks = parseWeeks(raw_weeks)
-            var name = raw_name.replace(/\(.*?\)/g,'') // 移除字符串中的所有()括号（包括其内容）
+            let weeks = parseWeeks(raw_weeks)
+            let name = raw_name.replace(/\(.*?\)/g, '') // 移除字符串中的所有()括号（包括其内容）
+            let token = name + weeks + day // 用来唯一标识一门课程信息
             // 判断是否记录过该门课程
-            var flag = -1
-            for (var i = 0; i < course.length; i++) {
-                if (course[i].token == name + weeks + day) {
+            let flag = -1
+            for (let i = 0; i < course.length; i++) {
+                if (course[i].token === token) {
                     flag = i
                 }
             }
-            if (flag == -1) {
+            if (flag === -1) {
                 // 记录新的课程信息
-                var obj = {
+                let obj = {
                     name: name,
                     position: position,
                     teacher: teacher,
                     weeks: weeks,
                     day: day,
                     sections: sections,
-                    token: name + weeks + day
+                    token: token
                 }
                 course.push(obj)
             } else {
@@ -128,16 +94,16 @@ function parseCourse(html) {
 /**
  * 解析课时信息
  *
- * @param  String  html 课表html
- * @return JSON
+ * @param  html 课表html
+ * @return Array[{},{}]
  */
 function parseSection(html) {
     let result = []
-    var i = 0
+    let i = 0
     // 遍历所有课程单元格
-    $('#jsTbl_01').find('tr').each(function() {
-        var time = $(this).find("td:nth-child(2)").text()
-        if (i != 0) {
+    $('#jsTbl_01').find('tr').each(function () {
+        let time = $(this).find("td:nth-child(2)").text()
+        if (i !== 0) {
             let section = {}
             section.section = i
             section.startTime = time.split('~')[0]
@@ -150,17 +116,52 @@ function parseSection(html) {
 }
 
 /**
- * 输入课程页面的HTML字符串，提取课程信息，按约定的格式输出JSON
+ * 转换课时信息为约定的JSON格式
  *
- * @param  String html 上一步函数获取到的html
- * @return JSON
+ * @param  array 课时列表
+ * @return Array[]
+ */
+function sectionsFormat(array) {
+    let result = []
+    for (let i = 0; i < array.length; i++) {
+        result.push({section: array[i]})
+    }
+    return result
+}
+
+/**
+ * 转换课程信息为约定的JSON格式
+ *
+ * @param  list 课程信息列表
+ * @return Array[]
+ */
+function courseFormat(list) {
+    let result = []
+    for (let i = 0; i < list.length; i++) {
+        let course = {}
+        course.name = list[i].name
+        course.position = list[i].position
+        course.teacher = list[i].teacher
+        course.weeks = list[i].weeks
+        course.day = list[i].day
+        course.sections = sectionsFormat(list[i].sections)
+        result.push(course)
+    }
+    return result
+}
+
+/**
+ * 输入课程页面的HTML字符串，提取课程信息，按约定的格式输出JSON字符串
+ *
+ * @param  html 上一步函数获取到的html
+ * @return JSON{}
  */
 function scheduleHtmlParser(html) {
     let raw_course = []
     let course = []
     let section = []
     raw_course = parseCourse(html)
-    course = course2JSON(raw_course)
+    course = courseFormat(raw_course)
     section = parseSection(html)
-    return { courseInfos: course, sectionTimes: section }
+    return {courseInfos: course, sectionTimes: section}
 }
